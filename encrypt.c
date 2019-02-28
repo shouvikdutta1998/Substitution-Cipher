bc4afc3754e434179ce685823fd3e9d6cbfdc272
shouvikdutta1998 <shouvikdutta123456789@gmail.com>
2019-02-28 18:11:58 +0530

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -1,15 +1,13 @@
 //A SIMPLE LOGICAL BASED SUBSTITUTION CIPHER ENCRYPTOGRAPHY PROGRAM WRITTEN IN C LANGUAGE
 #include<stdio.h>
-#include<conio.h>
-void main()
+int main()
   {
-  clrscr();
-  char s[50];
+   char s[50];
   printf("Enter any word to Encrypt\n"); //write a word here then enter
   scanf("%s",s);
   for(int i=0;i<=s[i];i++)
    {
      printf("%c",s[i]-2);     //The encrypted result will obtain here, Hence it is encrypted
    }
-    getch();
+    return 0;
    }
